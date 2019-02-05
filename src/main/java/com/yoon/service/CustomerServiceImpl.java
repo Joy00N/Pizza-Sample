b93ccecbd856520136864607ea006e138d315c0f
Joy <yoonhee.jo.cs@gmail.com>
2019-02-05 00:59:38 -0500

    init

--- src/main/java/com/yoon/service/CustomerServiceImpl.java
+++ src/main/java/com/yoon/service/CustomerServiceImpl.java
@@ -4,16 +4,26 @@ import java.util.List;
 
 import com.yoon.model.Customer;
 import com.yoon.repository.CustomerRepository;
-import com.yoon.repository.HibernateCustomerRepositoryImpl;
 
 public class CustomerServiceImpl implements CustomerService {
 
-	private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
-	/* (non-Javadoc)
-	 * @see com.yoon.service.CustomerService#findAll()
-	 */
+	private CustomerRepository customerRepository;
+
+	public CustomerServiceImpl() {
+		
+	}
+	
+	public CustomerServiceImpl(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+	
+	public void setCustomerRepository(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+	
 	@Override
 	public List<Customer> findAll(){
 		return customerRepository.findAll();
 	}
+	
 }
